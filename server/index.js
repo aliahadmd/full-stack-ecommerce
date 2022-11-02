5af812ce0119f08064ab25ce2c4b936fd3490935
Ali <90032081+iamaliahad@users.noreply.github.com>
2022-11-03 03:07:03 +0800

    deploy fixed

--- server/index.js
+++ server/index.js
@@ -30,9 +30,9 @@ app.use("/api", productRoutes);
 
 //deploy site
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
 );
 
 //---------
